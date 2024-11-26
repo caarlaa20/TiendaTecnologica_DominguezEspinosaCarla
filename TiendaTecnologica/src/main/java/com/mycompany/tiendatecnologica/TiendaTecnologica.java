b7c799696cd259525d8e1185479ca0c6cfaf0e09
Carla domínguez <carladominguez504@gmail.com>
2024-11-26 15:21:28 +0100

    he creado la clase donde voy a hacer mi tienda tecnoogíca

---
+++ TiendaTecnologica/src/main/java/com/mycompany/tiendatecnologica/TiendaTecnologica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tiendatecnologica;
+
+/**
+ *
+ * @author alumno
+ */
+public class TiendaTecnologica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
